5210a41efaf79025320d2984e070ddb4a12d2008
Yuka Takiyama <yukatakiyama27@gmail.com>
2020-02-19 14:09:46 +0900

    model and routes are set

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurant_attributes = [
+  {
+    name: 'Bistro Darble',
+    address: 'Ebisu',
+    phone_number: '03-1234-5678',
+    category: 'french'
+  },
+  {
+    name: 'Ulala',
+    address: 'Shibuya',
+    phone_number: '03-1234-5678',
+    category: 'italian'
+  },
+  {
+    name: 'Maruko',
+    address: 'Shibuya',
+    phone_number: '03-1234-5678',
+    category: 'japanese'
+  },
+  {
+    name: 'Craft Beer',
+    address: 'Meguro',
+    phone_number: '03-1234-5678',
+    category: 'belgian'
+  },
+  {
+    name: 'Dim Sum Haus',
+    address: 'frankfurt',
+    phone_number: '03-1234-5678',
+    category: 'chinese'
+  }
+]
+
+Restaurant.create!(restaurant_attributes)
